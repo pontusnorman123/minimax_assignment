aa1df22eb81df54875b0654b288205172bdd2ac6
ponorman <ponorman@kth.se>
2022-11-14 16:52:13 +0100

    test

---
+++ player.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+import random
+
+from fishing_game_core.game_tree import Node
+from fishing_game_core.player_utils import PlayerController
+from fishing_game_core.shared import ACTION_TO_STR
+
+
+class PlayerControllerHuman(PlayerController):
+    def player_loop(self):
+        """
+        Function that generates the loop of the game. In each iteration
+        the human plays through the keyboard and send
+        this to the game through the sender. Then it receives an
+        update of the game through receiver, with this it computes the
+        next movement.
+        :return:
+        """
+
+        while True:
+            # send message to game that you are ready
+            msg = self.receiver()
+            if msg["game_over"]:
+                return
+
+
+class PlayerControllerMinimax(PlayerController):
+
+    def __init__(self):
+        super(PlayerControllerMinimax, self).__init__()
+
+    def player_loop(self):
+        """
+        Main loop for the minimax next move search.
+        :return:
+        """
+
+        # Generate first message (Do not remove this line!)
+        first_msg = self.receiver()
+
+        while True:
+            msg = self.receiver()
+
+            # Create the root node of the game tree
+            node = Node(message=msg, player=0)
+
+            # Possible next moves: "stay", "left", "right", "up", "down"
+            best_move = self.search_best_next_move(initial_tree_node=node)
+
+            # Execute next action
+            self.sender({"action": best_move, "search_time": None})
+
+    def search_best_next_move(self, initial_tree_node):
+        """
+        Use minimax (and extensions) to find best possible next move for player 0 (green boat)
+        :param initial_tree_node: Initial game tree node
+        :type initial_tree_node: game_tree.Node
+            (see the Node class in game_tree.py for more information!)
+        :return: either "stay", "left", "right", "up" or "down"
+        :rtype: str
+        """
+
+        # EDIT THIS METHOD TO RETURN BEST NEXT POSSIBLE MODE USING MINIMAX ###
+
+        # NOTE: Don't forget to initialize the children of the current node
+        #       with its compute_and_get_children() method!
+
+        children = []
+        children = initial_tree_node.compute_and_get_children()
+        print(children[0].state.get_fish_scores())
+        random_move = random.randrange(5)
+        return ACTION_TO_STR[random_move]
+
+
+    def get_hueristic(node, depth):
+
+
+        i = 0
+        for j in node.children:
+            node[j].compute_and_get_children
+            if(i != depth):
+                i += 1
+                node.get_hueristic(node[i],depth)
+            node
