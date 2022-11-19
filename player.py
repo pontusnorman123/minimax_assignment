59d87be989bf8e6e5fc19bf37c884d514da740d7
EmilHardling <70025001+EmilHardling@users.noreply.github.com>
2022-11-19 18:31:51 +0100

    14 points

--- player.py
+++ player.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from math import inf
 import random
 
 from fishing_game_core.game_tree import Node
@@ -66,187 +65,70 @@ class PlayerControllerMinimax(PlayerController):
         # NOTE: Don't forget to initialize the children of the current node
         #       with its compute_and_get_children() method!
 
-        #print("fishes: ", initial_tree_node.state.get_fish_positions())
-        #print("hook:", initial_tree_node.state.get_hook_positions()[0])
-        #print("min_dis: ", self.create_hueristic(initial_tree_node))
-        random_move = random.randrange(5)
-        # print(ACTION_TO_STR[random_move])
-        startPlayer = 0
-        #self.get_hueristic(initial_tree_node, 2, 0)
-        # print(initial_tree_node.get_move())
+        nodes = initial_tree_node.compute_and_get_children()
 
-        return self.optimalMove(initial_tree_node, 4)
-        # return "left"
+        alpha = -999999
+        beta = 9999
+        highScore = -99999
+        best_move = 0
+
+        for node in nodes:
+            player = node.state.get_player()
+            score = self.minimax(node, 2, alpha, beta, player)
+            if (score > highScore):
+                highScore = score
+                best_move = node.move
+        return ACTION_TO_STR[best_move]
+
+    def minimax(self, node, depth, a, b, player):
+        if depth == 0 or node.compute_and_get_children() == []:
+            return self.evaluation(node)
+
+        nodes = node.compute_and_get_children()
+        if player == 0:
+            v = -99999
+            for child in nodes:
+                v = max(v, self.minimax(child, depth-1, a, b, 1))
+                a = max(a, v)
+                if b <= a:
+                    break
+            return v
+        else:
+            v = 99999
+            for child in nodes:
+                v = min(v, self.minimax(child, depth-1, a, b, 0))
+                b = min(b, v)
+                if a >= b:
+                    break
+            return v
 
     def computerBlocks(self, node, fishPos):
         hooks = node.state.get_hook_positions()
         computerPos = hooks[1][0]
         playerPos = hooks[0][0]
 
-        #print("FishPos:", fishPos, "  playerPos:", playerPos,"  compPos:", computerPos, "  block?", end=" ")
-        #print(min(fishPos, playerPos) <= computerPos <=max(fishPos, playerPos), end=" ")
-
         if (min(fishPos, playerPos) <= computerPos <= max(fishPos, playerPos)):
             return True
 
         return False
 
-    def closestFish(self, node, player):
-        fishes = node.state.get_fish_positions()
-        hookpos = node.state.get_hook_positions()[player]
-        minDistance = 666
-
-        """
-        
-            
-        """
-
-        for fish in fishes:
-            x = abs(hookpos[0]-fishes[fish][0])
-            if (self.computerBlocks(node, fishes[fish][0])):
-                x = 20-x
-            y = abs(hookpos[1]-fishes[fish][1])
-            if ((x+y) < minDistance):
-                minDistance = x+y
-            # print(x)
-            #print("hookpos:", hookpos, "   fishpos:",fishes[fish], "   x:", x, "   compPos:", node.state.get_hook_positions()[1])
-        return (1/(minDistance+1))
-
     def evaluation(self, node):
-        # Return poängställning
-        """
-        Input: hookPos, fishesPos, player.scores()
-        Function: Beräkna poängställning efter move
-        Output: Poängställning
-        """
-
-        p = abs(node.state.get_player_scores()[0])
-        c = node.state.get_player_scores()[1]
+        p = abs(node.state.get_player_scores()[0])  # Player points
+        c = node.state.get_player_scores()[1]       # Computer points
         heuristic = p-c
-        player = node.state.player
+        # Difference in points
+        return heuristic + self.decideFish(node)
 
-        #print("Heuristic", heuristic, "  p:", p, "  c:", c,"   fish:", self.closestFish(node, player))
-
-        #print(node.move, "  ", node.state.player_caught)
-        # min
-        if (player):
-            return heuristic - self.closestFish(node, player)
-        return heuristic + self.closestFish(node, player)
-
-    def minmax(self, node, depth, player):
-        #print("Depth:", depth, "   Player:", player)
-        if (depth == 0):
-            #print(ACTION_TO_STR[node.move], node.state.get_fish_positions(), node.state.get_hook_positions()[0])
-            return self.evaluation(node)
-
-        node.compute_and_get_children()  # Populate node with children
-
-        depth = depth - 1
-
-        if (player == 0):
-            player = 1
-            bestPossible = -inf
-            for child in node.children:
-                points = self.minmax(child, depth, player)
-                bestPossible = max(bestPossible, points)
-                #print(ACTION_TO_STR[child.move], points, "   ", end="")
-            #print("Depth:", node.depth, "   Player:", "0.   ", bestPossible)
-            return bestPossible
-        else:
-            bestPossible = inf
-            player = 0
-            for child in node.children:
-                points = self.minmax(child, depth, player)
-                #print("Move:", child.get_move(), "    Points:", points)
-                bestPossible = min(bestPossible, points)
-            #print("BestPossible:", bestPossible)
-            # print("----")
-            return bestPossible
-
-    """
-    Rules:
-    1. a/b pruning - "poäng skillnaden mellan p och c + poäng/fisk"
-    2. getMinDistance
-
-    1. Go for the best fish first
-    2. Position hook on same level as fish before moving boat
-    3. Move boat closer to fish
-
-    """
-
-    def highestFishScore(self, node):
+    def decideFish(self, node):
         fishes = node.state.get_fish_positions()
-        highScore = 0
-        bestFishes = []
-        for fish in fishes:
-            tempScore = node.state.get_fish_scores()[fish]
-            if tempScore > highScore:
-                highScore = tempScore
-                bestFishes.append(fish)
-        return bestFishes
-
-    def getDistance(self, playerPos, fishPos, movePos):
-        x = abs(playerPos[0]-fishPos[0] + movePos[0])
-        y = abs(playerPos[1]-fishPos[1] + movePos[1])
-        return x + y
-
-    def bestMove(self, node):
-        moves = {
-            "up": (0, 1), "down": (0, -1), "stay": (0, 0)}
         hookPos = node.state.get_hook_positions()[0]
-        fishesPos = node.state.get_fish_positions()
-        minDistance = 666
-        bestMove = "stay"
-        for fish in fishesPos:
-            for move in moves:
-                tempDistance = self.getDistance(
-                    hookPos, fishesPos[fish], moves[move])
-                #print("HookPos:", hookPos, "   ", "FishPos:", fishesPos[fish])
-                #print("distance:", tempDistance, "   ", "move", move)
-                if tempDistance < minDistance:
-                    minDistance = tempDistance
-                    targetFish = fish
-                    bestMove = move
-        return bestMove, minDistance
-
-    def minmaxMove(self, node, depth):
-        children = node.compute_and_get_children()
-        bestScore = -inf
-        bestMoves = 0
-        print("")
-        print(" --- Possible Scores ---")
-        for child in children:
-            # print(child.move)
-            score = self.minmax(child, depth-1, 0)
-            print(ACTION_TO_STR[child.move], score, "   ", end="")
-            if (score > bestScore):
-                bestScore = score
-                bestMoves = child.move
-        print("")
-        print("BestMove:", ACTION_TO_STR[bestMoves],
-              "    BestScore:", bestScore)
-        return ACTION_TO_STR[bestMoves]
-
-    def optimalMove(self, node, depth):
-        # GetHeuristic (skapar träd, kollar alla noder, hittar best outcome)
-
-        # Om det skiter sig (kan inte se tillräckligt långt fram)
-        # getMinDistance --> bestMove
-
-        # Return poängställningen i varje state
-        moves = {"left": (-1, 0), "right": (1, 0),
-                 "up": (0, 1), "down": (0, -1), "stay": (0, 0)}
-
-        min_dis = 666
-        bestMove = "stay"
-        bestFishes = self.highestFishScore(node)
-        #print("TargetedFish:", bestFishes)
-        moveCloser, distance = self.bestMove(node)
-        smartMove = self.minmaxMove(node, depth)
-        print("Distance:", distance)
-        print("Closer", moveCloser, "   Smart", smartMove)
-        #print("HooksPos:", node.state.get_hook_positions()[0])
-        # if (distance > depth/2):
-        # return moveCloser
-
-        return smartMove
+        ratio = 0
+        for f in fishes:
+            x = abs(hookPos[0]-fishes[f][0])
+            if (self.computerBlocks(node, fishes[f][0])):
+                x = abs(20-x)
+            y = abs(hookPos[1] - fishes[f][1])
+            d = x + y
+            point = abs(node.state.get_fish_scores()[f])
+            ratio = max(ratio, point/((d*d)+1))
+        return ratio
