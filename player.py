4888cb0e9c320d2d17d6f3e2415f66c398bee2a0
pontusnorman123 <pontusnorman1@gmail.com>
2022-11-27 09:59:58 +0100

    18p

--- player.py
+++ player.py
@@ -78,12 +78,47 @@ class PlayerControllerMinimax(PlayerController):
 
         for node in nodes:
             player = node.state.get_player()
-            score = self.minimax(node, 2, alpha, beta, player)
+            score = self.iter_depth_search(node, alpha, beta, player,highScore,7,0)
             if (score > highScore):
                 highScore = score
                 best_move = node.move
         return ACTION_TO_STR[best_move]
 
+    def iter_depth_search(self,node,a,b,player, prev_node_value,max_depth,cur_depth):
+
+        cur_depth = cur_depth + 1
+        if max_depth == cur_depth or node.compute_and_get_children() == []:
+            return self.evaluation(node)
+
+            ##Kollar om state finns i hash tabell
+        if (hash(node.state) in hash_table_states.keys()):
+            return hash_table_states.get(hash(node.state))
+
+        if player == 0:
+            current_node_value = self.evaluation(node)
+            v = -99999
+
+            #### Jämför om vi har bättre värde nu än den förra noden och om vi har det behöver vi inte gå djupare så vi returnar istället direkt.
+            if(current_node_value <= prev_node_value):
+                nodes = node.compute_and_get_children()
+                for child in nodes:
+                    v = max(v, self.iter_depth_search(child, a, b, 1,current_node_value,max_depth,cur_depth))
+                    a = max(a, v)
+                    if b <= a:
+                        break
+                return v
+            else:
+                return current_node_value
+        else:
+            current_node_value = self.evaluation_p1(node)
+            v = 99999
+            nodes = node.compute_and_get_children()
+            for child in nodes:
+                v = min(v, self.iter_depth_search(child, a, b, 0,current_node_value,max_depth,cur_depth))
+                b = min(b, v)
+                if a >= b:
+                    break
+            return v
     def minimax(self, node, depth, a, b, player):
         if depth == 0 or node.compute_and_get_children() == []:
             return self.evaluation(node)
@@ -125,6 +160,15 @@ class PlayerControllerMinimax(PlayerController):
         c = node.state.get_player_scores()[1]       # Computer points
         heuristic = p-c
         # Difference in points
+        if( hash(node.state) not in hash_table_states.keys() and node.state.player == 0):
+            self.add_state_to_hash_table(hash(node.state),heuristic + self.decideFish(node)) ## add state with value to hash table
+        return heuristic + self.decideFish(node)
+
+    def evaluation_p1(self,node):
+        p = abs(node.state.get_player_scores()[0])  # Player points
+        c = node.state.get_player_scores()[1]       # Computer points
+        heuristic = c-p
+        # Difference in points
         self.add_state_to_hash_table(hash(node.state),heuristic + self.decideFish(node)) ## add state with value to hash table
         return heuristic + self.decideFish(node)
 
@@ -139,6 +183,7 @@ class PlayerControllerMinimax(PlayerController):
             y = abs(hookPos[1] - fishes[f][1])
             d = x + y
             point = abs(node.state.get_fish_scores()[f]) ##Ska det vara abs om vissa fiskar har negativ poäng
+            point = node.state.get_fish_scores()[f] ##Ska det vara abs om vissa fiskar har negativ poäng
             ratio = max(ratio, point/((d*d)+1))
         return ratio
 
